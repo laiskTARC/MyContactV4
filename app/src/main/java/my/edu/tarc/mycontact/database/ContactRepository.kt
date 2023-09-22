4b6d3de581af04743be0e8f15469a25ce58f94bf
Lai Siu Kit <laisk-wm20@student.tarc.edu.my>
2023-09-22 16:05:46 +0800

    Updated

--- app/src/main/java/my/edu/tarc/mycontact/database/ContactRepository.kt
+++ app/src/main/java/my/edu/tarc/mycontact/database/ContactRepository.kt
@@ -2,6 +2,8 @@ package my.edu.tarc.mycontact.database
 
 import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import my.edu.tarc.mycontact.ui.contact_list.Contact
 
 class ContactRepository(private val contactDao: ContactDao) {
@@ -26,5 +28,18 @@ class ContactRepository(private val contactDao: ContactDao) {
     @WorkerThread
     suspend fun update(contact: Contact){
         contactDao.update(contact)
+    }
+
+    @WorkerThread
+    suspend fun uploadContactList(contact_id: String){
+        val firebaseDatabase = Firebase.database
+        val myRef = firebaseDatabase.getReference("profile")
+        for(contact in allContacts.value?.iterator()!!){
+            with(myRef.child(contact_id).child("contact_list")){
+                child(contact.phone).child("phone").setValue(contact.name)
+                child(contact.phone).child("phone").setValue(contact.email)
+                child(contact.phone).child("phone").setValue(contact.phone)
+            }
+        }
     }
 }
